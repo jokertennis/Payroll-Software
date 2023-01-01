a796aaf03f0f1c598ca5f477aec2e5fdd32c7d96
Kenji Kawata <kenjikawata@KenjinoMacBook-Air.local>
2023-01-01 23:46:03 +0900

    Refactored.

---
+++ db/migrate.go
@@ -0,0 +1,31 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/mysql"
+)
+
+// Reference : https://github.com/golang-migrate/migrate/tree/master/database/mysql#use-with-existing-client
+func CreateMigrateInstance() (*migrate.Migrate, error) {
+	// https://github.com/go-sql-driver/mysql/#multistatements
+	db, err := sql.Open("mysql", "root:password@tcp(db_container:3306)/develop_db")
+	if err != nil {
+		return nil, fmt.Errorf("sql.Open error. err:%s", err)
+	}
+	driver, err := mysql.WithInstance(db, &mysql.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to create driver. err:%s", err)
+	}
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://src/ddl",
+		"mysql",
+		driver,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create migration-instance. err:%s", err)
+	}
+
+	return m, nil
+}
--- main.go
+++ main.go
@@ -1,17 +1,15 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
+	"usr/local/go/db"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/golang-migrate/migrate/v4"
-	"github.com/golang-migrate/migrate/v4/database/mysql"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
 func main() {
-	migrateInstance, errorCreateMigrateInstance := createMigrateInstance()
+	migrateInstance, errorCreateMigrateInstance := db.CreateMigrateInstance()
 	if errorCreateMigrateInstance != nil {
 		fmt.Printf("failed to create MigrateInstance. err:%s", errorCreateMigrateInstance)
 	}
@@ -19,27 +17,4 @@ func main() {
 	if err := migrateInstance.Up(); err != nil {
 		fmt.Printf("failed to up. err:%s", err)
 	}
-}
-
-// Reference : https://github.com/golang-migrate/migrate/tree/master/database/mysql#use-with-existing-client
-func createMigrateInstance() (*migrate.Migrate, error) {
-	// https://github.com/go-sql-driver/mysql/#multistatements
-	db, err := sql.Open("mysql", "root:password@tcp(db_container:3306)/develop_db")
-	if err != nil {
-		return nil, fmt.Errorf("sql.Open error. err:%s", err)
-	}
-	driver, err := mysql.WithInstance(db, &mysql.Config{})
-	if err != nil {
-		return nil, fmt.Errorf("failed to create driver. err:%s", err)
-	}
-	m, err := migrate.NewWithDatabaseInstance(
-		"file://db/migrations",
-		"mysql",
-		driver,
-	)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create migration-instance. err:%s", err)
-	}
-
-	return m, nil
 }
