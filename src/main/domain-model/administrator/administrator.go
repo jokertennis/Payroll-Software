8dbb94d663f8ca77b09d0abc169d640c438f6d9d
Kenji Kawata <kenji.k.4506@gmail.com>
2023-04-20 12:08:21 +0900

    Created CreateSalaryStatement function.

--- src/main/domain-model/administrator/administrator.go
+++ src/main/domain-model/administrator/administrator.go
@@ -1,5 +1,11 @@
 package administrator
 
+import (
+	"fmt"
+	"usr/local/go/src/main/domain-model/salary_statement"
+	"usr/local/go/src/main/domain-service/repository/salary_statement_repository"
+)
+
 type Administrator struct {
 	ID          uint32
 	CompanyId   uint16
@@ -11,4 +17,12 @@ type Administrator struct {
 // Not yet created value object for each attribute.
 func NewAdministrator(id uint32, companyId uint16, name string, mailAddress string, password string) (*Administrator, error) {
 	return &Administrator{id, companyId, name, mailAddress, password}, nil
+}
+
+func CreateSalaryStatementByUsingIndividualData(salaryStatementRepository salary_statement_repository.SalaryStatementRepository, salaryStatementEntry salary_statement.SalaryStatementEntryByUsingIndividualDatas) (salaryStatementId uint32, err error) {
+	salaryStatementId, err = salaryStatementRepository.CreateSalaryStatement(salaryStatementEntry)
+	if err != nil {
+		return 0, fmt.Errorf("failed to create salary statement.error:%s", err)
+	}
+	return salaryStatementId, nil
 }
--- src/main/domain-service/repository/salary_statement_repository/salary_statement_repository.go
+++ src/main/domain-service/repository/salary_statement_repository/salary_statement_repository.go
@@ -8,4 +8,5 @@ import (
 type SalaryStatementRepository interface {
 	GetSalaryStatement(employeeId uint32, yearOfPayday int, monthOfPayday time.Month) (*salary_statement.SalaryStatement, error)
 	GetAllSalaryStatements(employeeId uint32) ([]*salary_statement.SalaryStatement, error)
+	CreateSalaryStatement(salaryStatementEntry salary_statement.SalaryStatementEntryByUsingIndividualDatas) (salaryStatementId uint32, err error)
 }
