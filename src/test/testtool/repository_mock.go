2933e2c437c92c2217b667534fa273058d274e55
Kenji Kawata <kenji.k.4506@gmail.com>
2023-04-20 12:19:42 +0900

    Fixed repository_mock file to pass all tests.

--- src/test/testtool/repository_mock.go
+++ src/test/testtool/repository_mock.go
@@ -21,6 +21,7 @@ type SalaryStatementRepositoryMock struct {
 	SalaryStatementRepository  salary_statement_repository.SalaryStatementRepository
 	FakeGetSalaryStatement     func(employeeId uint32, yearOfPayday int, monthOfPayday time.Month) (*salary_statement.SalaryStatement, error)
 	FakeGetAllSalaryStatements func(employeeId uint32) ([]*salary_statement.SalaryStatement, error)
+	FakeCreateSalaryStatement  func(salaryStatementEntry salary_statement.SalaryStatementEntryByUsingIndividualDatas) (salaryStatementId uint32, err error)
 }
 
 func (m *SalaryStatementRepositoryMock) GetSalaryStatement(employeeId uint32, yearOfPayday int, monthOfPayday time.Month) (*salary_statement.SalaryStatement, error) {
@@ -29,4 +30,8 @@ func (m *SalaryStatementRepositoryMock) GetSalaryStatement(employeeId uint32, ye
 
 func (m *SalaryStatementRepositoryMock) GetAllSalaryStatements(employeeId uint32) ([]*salary_statement.SalaryStatement, error) {
 	return m.FakeGetAllSalaryStatements(employeeId)
+}
+
+func (m *SalaryStatementRepositoryMock) CreateSalaryStatement(salaryStatementEntry salary_statement.SalaryStatementEntryByUsingIndividualDatas) (salaryStatementId uint32, err error) {
+	return 
 }
